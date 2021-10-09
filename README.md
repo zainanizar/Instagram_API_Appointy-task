33c5e7b60c30b7794729eeb44b9a3804e0233aca
zaina nizar <nizarzaina2@gmail.com>
2021-10-09 19:25:05 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# appointy_task
+# Instagram Backend API
+
+<p align="center"><img width=100% src="https://raw.githubusercontent.com/ahmdrz/goinsta/v1/resources/goinsta-image.png"></p>
--- main_test.go
+++ main_test.go
@@ -8,20 +8,11 @@ type user struct {
 	z string	
 	a string	
 }
-type Post struct {
-	b int
-	c string	
-	d string	
-		
-}
 
 var getSession = []GetUser{
 	{1, "zaina", "zaina", "zaina123"}
 }
 
-var getSession = []GetPost{
-	{1, "zaina", "zaina"}
-}
 
 func TestgetUser(t *testing.T) {
 	for _, test := range getSession {
@@ -31,3 +22,5 @@ func TestgetUser(t *testing.T) {
 		}
 	}
 }
+
+
